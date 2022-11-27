bf9d00e753cf8ecea445e2823a046827f0db62d1
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-11-27 13:30:45 +0200

    Add project files.

---
+++ Hârdea Georgiana L1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Hârdea_Georgiana_L1
+{
+    class Program
+    {
+        /* E1.Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+           paralelipiped dreptunghic, va afisa volumul lui*/
+        static void Main(string[] args)
+        {
+            int lungime;
+            int latime;
+            int inaltime;
+           
+            Console.WriteLine("Introduceti lungimea");
+         
+            lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latimea");
+            latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltimea");
+            inaltime = int.Parse(Console.ReadLine());
+
+            int volumulParalelipipedDreptunghic = lungime * latime * inaltime;
+
+            Console.WriteLine("Aria paralelipipedului dreptunghic este " + volumulParalelipipedDreptunghic);
+
+           
+            
+        }
+    }
+}
